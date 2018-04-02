eeeebf1274048f53358e8cc8852b240bb962829d
ОстроуховМ <@223_10>
2018-04-02 22:06:23 +0600

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KKB.Bank.Module;
+using GeneratorName;
+
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Client> ListClient = new List<Client>();
+                GeneratorName.Generator gn = new Generator();
+                Client client = new Client();
+                client.DoB = DateTime.Now.AddYears(-60);
+                client.FullName = gn.GenerateDefault(Gender.man);
+                client.IIN = "123456789123";
+                client.Login = "Qwerty";
+                client.Password = "123";
+                client.PhoneNumber = "87651684585";
+                ListClient.Add(client);
+
+            }
+            catch(System.Net.WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ KKB.Bank/Module/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KKB.Bank.Module
+{
+    public class Client
+    {
+        public Client()
+        {
+            ListAccount = new List<Account>();
+        }
+
+      public string FullName { get; set; }
+
+        private string IIN_;
+
+      public string IIN {
+            get
+            {
+                return IIN_;
+            }
+            set { if (value.Length == 12)
+                {
+                    IIN_ = value;
+
+                }
+            else
+                {
+                    throw new Exception("Invalid sequence of IIN");
+
+                }
+
+            }
+
+        }   
+        public DateTime DoB { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+        public string Login { get;  set; }
+        public string Password { get; set; }
+
+        List<Account> ListAccount;
+
+
+    }
+}
